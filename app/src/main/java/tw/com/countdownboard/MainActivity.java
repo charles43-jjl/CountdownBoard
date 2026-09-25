740f3cce082e65279e67aa817e8e63a5de55d289
charles43-jjl <charles4348@gmail.com>
2026-09-25 10:17:15 +0800

    Fix recurring event Java formatting

--- app/src/main/java/tw/com/countdownboard/MainActivity.java
+++ app/src/main/java/tw/com/countdownboard/MainActivity.java
@@ -11,7 +11,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;\nimport android.widget.Spinner;\nimport android.widget.ArrayAdapter;
+import android.widget.ScrollView;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
